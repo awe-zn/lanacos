0bfd4169e5114ce812532d3b8587098c5103aae0
Daniel Souza <eu@danielsouza.dev.br>
2021-09-29 03:52:30 -0300

    feat(database): seeding database models using JSON

---
+++ database/seeders/County.ts
@@ -0,0 +1,12 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder';
+
+import County from 'App/Models/County';
+
+import counties from '../json/counties.json';
+
+export default class CountySeeder extends BaseSeeder {
+  public async run() {
+    await County.truncate(true);
+    await County.createMany(counties);
+  }
+}
