ffca2936dfe93b0d4bf61eb8db6b11e1a3bef2f3
Daniel Souza <eu@danielsouza.dev.br>
2021-11-03 04:02:19 -0300

    fix(database): fix lenght of name's column of table companies, from 32 to 64

--- database/migrations/1632867109305_create_companies_table.ts
+++ database/migrations/1632867109305_create_companies_table.ts
@@ -6,7 +6,7 @@ export default class Companies extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id');
-      table.string('name', 32).notNullable();
+      table.string('name', 64).notNullable();
       table.string('description').notNullable();
       table
         .integer('manager_id')
