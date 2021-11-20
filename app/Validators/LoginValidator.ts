749546bcf129932a666d3f4bae742f4138b40b2a
Daniel Barbosa de Souza <51936265+SouzaDaniel@users.noreply.github.com>
2021-11-20 10:49:32 -0300

    Revert "refactor(validator): removing useless contructor from validator class"
    This reverts commit 9a2e26fe0d75e387f0586a7dad60a105792fef73.

--- app/Validators/LoginValidator.ts
+++ app/Validators/LoginValidator.ts
@@ -1,6 +1,9 @@
 import { schema, rules } from '@ioc:Adonis/Core/Validator';
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 
 export class LoginValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
   public schema = schema.create({
     email: schema.string({ trim: true }, [
       rules.email(),
