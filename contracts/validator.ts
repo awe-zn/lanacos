af0ce1441816fe257b6656932f09e9db6ff58a2f
Daniel Barbosa de Souza <51936265+SouzaDaniel@users.noreply.github.com>
2021-11-20 10:49:32 -0300

    Revert "feat(validator): creation of new validation classes and improving previous validators"
    This reverts commit b6ac442903d3374d79175feaf076970affc0a2d0.

--- contracts/validator.ts
+++ contracts/validator.ts
@@ -1,12 +1,5 @@
 declare module '@ioc:Adonis/Core/Validator' {
   interface Rules {
-    /**
-     * Value must be different of `compareString`
-     */
     notEqualTo(compareString: string): Rule;
-    /**
-     * Value should be a valid format `CPF`
-     */
-    cpf(): Rule;
   }
 }
--- start/validator.ts
+++ start/validator.ts
@@ -1,5 +1,4 @@
 import { validator } from '@ioc:Adonis/Core/Validator';
-import { cpf } from 'cpf-cnpj-validator';
 
 validator.rule('notEqualTo', (value, [compareString], options) => {
   if (typeof value !== 'string') {
@@ -10,23 +9,8 @@ validator.rule('notEqualTo', (value, [compareString], options) => {
     options.errorReporter.report(
       options.pointer,
       'notEqualTo',
-      'notEqualTo validation failed'
+      'notEqualTo validation failed',
+      options.arrayExpressionPointer
     );
   }
-});
-
-validator.rule('cpf', (value, _, options) => {
-  if (typeof value !== 'string') {
-    return;
-  }
-
-  if (!cpf.isValid(value)) {
-    options.errorReporter.report(
-      options.pointer,
-      'cpf',
-      'cpf validation failed'
-    );
-  }
-
-  options.mutate(cpf.strip(value));
 });
