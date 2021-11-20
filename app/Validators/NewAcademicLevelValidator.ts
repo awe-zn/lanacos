58698a16e548bf492dc8298aba3d490efe1e6b2a
Daniel Barbosa de Souza <51936265+SouzaDaniel@users.noreply.github.com>
2021-11-20 10:49:32 -0300

    Revert "feat: making complete CRUD of academic levels"
    This reverts commit 0046d799eb3d3ae3425300f7d184a3f7ec0e31cf.

--- app/Validators/NewAcademicLevelValidator.ts
+++
@@ -1,9 +0,0 @@
-import { schema } from '@ioc:Adonis/Core/Validator';
-
-export default class NewAcademicLevelValidator {
-  public schema = schema.create({
-    name: schema.string({ trim: true }),
-  });
-
-  public messages = {};
-}
