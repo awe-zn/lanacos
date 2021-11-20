43e4e8ebd173a04c81bee21135d56244115874d4
Daniel Barbosa de Souza <51936265+SouzaDaniel@users.noreply.github.com>
2021-11-20 10:49:32 -0300

    Revert "refactor(middleware): refactor middleware's verifiedEmail"
    This reverts commit b92e88097d7454c2221750deb34de49fb0fb4ef8.

--- app/Middleware/VerifiedEmail.ts
+++ app/Middleware/VerifiedEmail.ts
@@ -7,10 +7,11 @@ export default class VerifiedEmail {
   ) {
     const { emailConfirmed } = auth.user!;
 
-    if (!emailConfirmed)
+    if (!emailConfirmed) {
       return response.unauthorized({
         errors: [{ message: 'Email not confirmed' }],
       });
+    }
 
     return next();
   }
